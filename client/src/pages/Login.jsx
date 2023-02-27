5bb8c4ac44ee90906512f98b47dc597b2adf079c
Salman Hamza <90480347+salmanhamza@users.noreply.github.com>
2023-02-27 18:20:15 +0500

    commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
+const defaultValue = {
+  name: "",
+  email: "",
+  password: "",
+};
 const Login = () => {
+  const [user, setUser] = useState(defaultValue);
+
+  const onValueChange = (e) => {};
   return (
     <Container>
       <Row>
@@ -15,24 +23,30 @@ const Login = () => {
           <Form style={{ width: "80%", minWidth: 500 }}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                onChange={onValueChange}
+              />
+              <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={onValueChange}
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Submit
+              Login
             </Button>
             <div className="py-4">
-              <p>
+              <p className="text-center">
                 Don't have an Account <Link to={"/signup"}>Sign Up</Link>
               </p>
             </div>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Signup.css";
+import bot from "../assets/bot.jpeg";
+
+const defaultValue = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 const Signup = () => {
+  const [user, setUser] = useState(defaultValue);
+
+  // for image upload
+  const [image, setImage] = useState(null);
+  const [uploadingImg, setUploadingImg] = useState(false);
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const onValueChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+
+  const validateImg = (e) => {
+    const file = e.target.files[0];
+    if (file.size >= 1048576) {
+      return alert("file size must be less than 1mb");
+    } else {
+      setImage(file);
+      setImagePreview(URL.createObjectURL(file));
+    }
+  };
+
+  const uploadImage = async () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "qojckfyv");
+    try {
+      setUploadingImg(true);
+      let res = await fetch(
+        "https://api.claudinary.com/v1_1/salmanhamza/image/upload",
+        {
+          method: "post",
+          body: data,
+        }
+      );
+      const urlData = await res.json();
+      setUploadingImg(false);
+      return urlData.url;
+    } catch (error) {
+      setUploadingImg(false);
+      console.log(error);
+    }
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    if (!image) return alert("please upload your profile picture");
+    const url = await uploadImage(image);
+    console.log(url);
+  };
   return (
     <Container>
       <Row>
@@ -11,27 +68,62 @@ const Signup = () => {
           md={7}
           className="d-flex align-items-center justify-content-center flex-diresction-column"
         >
-          <Form style={{ width: "80%", minWidth: 500 }}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form style={{ width: "80%", minWidth: 500 }} onSubmit={onSubmitForm}>
+            <h1 className="text-center">Create Account</h1>
+            <div className="signup-profile-pic__container">
+              <img
+                className="signup-profile-pic"
+                src={imagePreview || bot}
+                alt="profile picture"
+              />
+              <label htmlFor="image-upload" className="add-picture-icon">
+                <i className="fas fa-plus-circle add-picture-icon"></i>
+              </label>
+              <input
+                type="file"
+                name=""
+                id="image-upload"
+                hidden
+                accept="image/jpg , image/jpeg"
+                onChange={validateImg}
+              />
+            </div>
+            <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Name" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                onChange={onValueChange}
+                name="name"
+              />
             </Form.Group>
 
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                onChange={onValueChange}
+                name="email"
+              />
+            </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={onValueChange}
+                name="password"
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Submit
+              Create Account
             </Button>
             <div className="py-4">
-              <p>
+              <p className="text-center">
                 Already have an Account? <Link to={"/login"}>Login</Link>
               </p>
             </div>
